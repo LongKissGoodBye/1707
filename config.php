c2c4be0d07e0a07733f57284d8d6dde26134dd98
LongKissGoodBye <2228571049@qq.com>
2017-12-22 09:36:35 +0800

    1

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 @header("content-type:text/html;charset=UTF-8");
 @header("Access-Control-Allow-Origin:*");
 // $connect = mysql_connect("http://120.78.94.207:3306","root","root");
-$conn = mysql_connect("localhost:3306","root","root");
+$conn = mysql_connect("120.78.94.207:3306","root","root");
 
 mysql_select_db('1707');
 
